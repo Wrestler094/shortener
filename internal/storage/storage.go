8da04a4a50fb920a1909ca66aa4f1a7c043a1c1e
Mikhail Kovalev <kovalev094@gmail.com>
2025-02-19 12:17:31 +0300

    [Iter1]: Реализация первого инкремента с временными решениями (#1)
    * [Iter1]: Реализация первого инкремента с временными решениями

---
+++ internal/storage/storage.go
@@ -0,0 +1,22 @@
+package storage
+
+import "sync"
+
+var (
+	// map[shortenURL]URL
+	storage = make(map[string]string)
+	mu      sync.RWMutex
+)
+
+func Save(shortenURL string, url string) {
+	mu.Lock()
+	storage[shortenURL] = url
+	mu.Unlock()
+}
+
+func Get(shortenURL string) (string, bool) {
+	mu.RLock()
+	url, ok := storage[shortenURL]
+	mu.RUnlock()
+	return url, ok
+}
