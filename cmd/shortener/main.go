ec9ce501a030766cd40008b51063b92e8aa1c0bf
Mikhail Kovalev <kovalev094@gmail.com>
2025-02-21 11:23:41 +0700

    [Iter4]: Fix address

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -13,7 +13,7 @@ import (
 
 func parseFlags() {
 	flag.StringVar(&configs.FlagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&configs.FlagBaseAddr, "b", "http://localhost:8080/", "basic address and port of result url")
+	flag.StringVar(&configs.FlagBaseAddr, "b", "http://localhost:8080", "basic address and port of result url")
 	flag.Parse()
 }
 
