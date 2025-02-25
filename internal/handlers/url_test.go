5629b2b52529150e3c1f3eed308c7fe019ee1f35
Mikhail Kovalev <kovalev094@gmail.com>
2025-02-25 14:22:58 +0700

    [Iter7]: Исправление тестов

--- internal/handlers/url_test.go
+++ internal/handlers/url_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSaveURL(t *testing.T) {
+func TestSavePlainURL(t *testing.T) {
 	type want struct {
 		code        int
 		contentType string
@@ -46,7 +46,58 @@ func TestSaveURL(t *testing.T) {
 			request := httptest.NewRequest(http.MethodPost, "/", body)
 			// создаём новый Recorder
 			w := httptest.NewRecorder()
-			SaveURL(w, request)
+			SavePlainURL(w, request)
+
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, test.want.code, res.StatusCode)
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			assert.NotEqual(t, "", string(resBody))
+			assert.Contains(t, res.Header.Get("Content-Type"), test.want.contentType)
+		})
+	}
+}
+
+func TestSaveJsonURL(t *testing.T) {
+	type want struct {
+		code        int
+		contentType string
+		bodyContent string
+	}
+
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "positive test #1",
+			want: want{
+				code:        201,
+				contentType: "application/json",
+				bodyContent: `{"url": "http://yandex.ru"}`,
+			},
+		},
+		{
+			name: "negative test #1",
+			want: want{
+				code:        400,
+				contentType: "text/plain",
+				bodyContent: `{"url": "yandex.ru"}`,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			body := strings.NewReader(test.want.bodyContent)
+			request := httptest.NewRequest(http.MethodPost, "/api/shorten", body)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			SaveJsonURL(w, request)
 
 			res := w.Result()
 			// проверяем код ответа
