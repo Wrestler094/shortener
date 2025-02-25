d9e2de304be1342302355bf148501a44b2335dc8
Mikhail Kovalev <kovalev094@gmail.com>
2025-02-25 10:26:38 +0300

    [Iter7]: Добавление encoder (#7)
    * [Iter6]: Добавление encoder

--- internal/handlers/url.go
+++ internal/handlers/url.go
@@ -1,38 +1,58 @@
 package handlers
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"strings"
 
 	"github.com/Wrestler094/shortener/internal/configs"
+	"github.com/Wrestler094/shortener/internal/services"
 	"github.com/Wrestler094/shortener/internal/storage"
-	"github.com/Wrestler094/shortener/internal/utils"
 )
 
-func SaveURL(res http.ResponseWriter, req *http.Request) {
+type ShortenRequest struct {
+	URL string `json:"url"`
+}
+
+type ShortenResponse struct {
+	Result string `json:"result"`
+}
+
+func SaveJSONURL(res http.ResponseWriter, req *http.Request) {
+	var shortenRequest ShortenRequest
+
+	err := json.NewDecoder(req.Body).Decode(&shortenRequest)
+	if err != nil {
+		http.Error(res, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	shortID, err := services.SaveURL(shortenRequest.URL)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusCreated)
+	json.NewEncoder(res).Encode(ShortenResponse{
+		Result: configs.FlagBaseAddr + "/" + shortID,
+	})
+}
+
+func SavePlainURL(res http.ResponseWriter, req *http.Request) {
 	body, err := io.ReadAll(req.Body)
 	if err != nil || len(body) == 0 {
 		http.Error(res, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-	originalURL := strings.TrimSpace(string(body))
 
-	if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
-		http.Error(res, "Invalid URL format", http.StatusBadRequest)
-		return
-	}
-
-	shortID, err := utils.GenerateShortID()
+	shortID, err := services.SaveURL(string(body))
 	if err != nil {
-		http.Error(res, "Failed to generate short URL", http.StatusBadRequest)
-		return
+		http.Error(res, err.Error(), http.StatusBadRequest)
 	}
 
-	// TODO: Сделать првоерку на случай если id или URL уже существует
-	storage.Save(shortID, originalURL)
-
-	res.Header().Set("content-type", "text/plain")
+	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusCreated)
 	res.Write([]byte(configs.FlagBaseAddr + "/" + shortID))
 }
@@ -42,13 +62,13 @@ func GetURL(res http.ResponseWriter, req *http.Request) {
 	urlParts = urlParts[1:]
 
 	if len(urlParts) != 1 || urlParts[0] == "" {
-		http.Error(res, "Request without id of shorten url", http.StatusBadRequest)
+		http.Error(res, "request without id of shorten url", http.StatusBadRequest)
 		return
 	}
 
 	url, ok := storage.Get(urlParts[0])
 	if !ok {
-		http.Error(res, "Shorten URL not found", http.StatusBadRequest)
+		http.Error(res, "shorten URL not found", http.StatusBadRequest)
 	}
 
 	res.Header().Set("Location", url)
---
+++ internal/services/url.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Wrestler094/shortener/internal/storage"
+	"github.com/Wrestler094/shortener/internal/utils"
+)
+
+func SaveURL(url string) (string, error) {
+	originalURL := strings.TrimSpace(url)
+
+	if !strings.HasPrefix(originalURL, "http://") && !strings.HasPrefix(originalURL, "https://") {
+		return "", errors.New("invalid URL format")
+	}
+
+	shortID, err := utils.GenerateShortID()
+	if err != nil {
+		return "", errors.New("failed to generate short id")
+	}
+
+	// TODO: Сделать првоерку на случай если id или URL уже существует
+	storage.Save(shortID, originalURL)
+	return shortID, nil
+}
