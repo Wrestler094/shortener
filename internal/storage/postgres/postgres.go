d9d7144be419b4227f2dfab7bcc31bfa4bed8c36
Mikhail Kovalev <kovalev094@gmail.com>
2025-04-16 19:06:59 +0700

    [Iter11]: Refactor

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -7,6 +7,8 @@ import (
 	"log"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.uber.org/zap"
 
